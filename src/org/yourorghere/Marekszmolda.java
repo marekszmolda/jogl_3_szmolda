5fe3533387a9a5f71bca21392adf37f3ec36a10b
student <student@240-06.INFORMATYKA.local>
2016-11-10 14:35:26 +0100

    las

--- src/org/yourorghere/Marekszmolda.java
+++ src/org/yourorghere/Marekszmolda.java
@@ -1,6 +1,5 @@
 package org.yourorghere;
 
-
 import com.sun.opengl.util.Animator;
 import java.awt.Frame;
 import java.awt.event.KeyEvent;
@@ -13,23 +12,23 @@ import javax.media.opengl.GLCanvas;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.glu.GLU;
 
-
-
 /**
  * Projekt1.java <BR>
- * author: Brian Paul (converted to Java by Ron Cemer and Sven Goethel) <P>
+ * author: Brian Paul (converted to Java by Ron Cemer and Sven Goethel)
+ * <P>
  *
  * This version is equal to Brian Paul's version 1.2 1999/10/21
  */
 public class Marekszmolda implements GLEventListener {
 //statyczne pola okre?laj?ce rotacj? wokó? osi X i Y
- private static float xrot = 0.0f, yrot = 0.0f;
 
-   public  static float ambientLight[] = { 0.3f, 0.3f, 0.3f, 1.0f };//swiat?o otaczaj?ce
-   public     static float diffuseLight[] = { 0.7f, 0.7f, 0.7f, 1.0f };//?wiat?o rozproszone
-   public      static float specular[] = { 1.0f, 1.0f, 1.0f, 1.0f}; //?wiat?o odbite
-   public       static float lightPos[] = { 0.0f, 150.0f, 150.0f, 1.0f };//pozycja ?wiat?a
- 
+    private static float xrot = 0.0f, yrot = 0.0f;
+
+    public static float ambientLight[] = {0.3f, 0.3f, 0.3f, 1.0f};//swiat?o otaczaj?ce
+    public static float diffuseLight[] = {0.7f, 0.7f, 0.7f, 1.0f};//?wiat?o rozproszone
+    public static float specular[] = {1.0f, 1.0f, 1.0f, 1.0f}; //?wiat?o odbite
+    public static float lightPos[] = {0.0f, 150.0f, 150.0f, 1.0f};//pozycja ?wiat?a
+
     public static void main(String[] args) {
         Frame frame = new Frame("Simple JOGL Application");
         GLCanvas canvas = new GLCanvas();
@@ -55,55 +54,57 @@ public class Marekszmolda implements GLEventListener {
             }
         });
         //Obs?uga klawiszy strza?ek
- frame.addKeyListener(new KeyListener()
- {
- public void keyPressed(KeyEvent e)
- {
- if(e.getKeyCode() == KeyEvent.VK_UP)
- xrot -= 1.0f;
- if(e.getKeyCode() == KeyEvent.VK_DOWN)
- xrot +=1.0f;
- if(e.getKeyCode() == KeyEvent.VK_RIGHT)
- yrot += 1.0f;
- if(e.getKeyCode() == KeyEvent.VK_LEFT)
- yrot -=1.0f;
- 
- 
- if(e.getKeyChar()=='q'){
-     ambientLight= new float[]{ambientLight[0]-0.1f, ambientLight[1]-0.1f,ambientLight[2]-0.1f,ambientLight[3]-0.01f};
-     }
-  if(e.getKeyChar()=='w'){
-     ambientLight= new float[]{ambientLight[0]+0.1f, ambientLight[1]+0.1f,ambientLight[2]+0.1f,ambientLight[3]+0.01f};
-     }
-  if(e.getKeyChar()=='a'){
-     diffuseLight= new float[]{diffuseLight[0]-0.1f, diffuseLight[1]-0.1f,diffuseLight[2]-0.1f,diffuseLight[3]-0.01f};
-     }
-  if(e.getKeyChar()=='s'){
-     diffuseLight= new float[]{diffuseLight[0]+0.1f, diffuseLight[1]+0.1f,diffuseLight[2]+0.1f,diffuseLight[3]+0.01f};
-     }
-  if(e.getKeyChar()=='z'){
-     specular= new float[]{specular[0]-0.1f, specular[1]-0.1f,specular[2]-0.1f,specular[3]-0.01f};
-     }
-  if(e.getKeyChar()=='x'){
-     specular= new float[]{specular[0]+0.1f, specular[1]+0.1f,specular[2]+0.1f,specular[3]+0.01f};
-     }
-  if(e.getKeyChar()=='k'){
-     lightPos= new float[]{lightPos[0]-0.1f, lightPos[1]-0.1f,lightPos[2]-0.1f,lightPos[3]-0.01f};
-     }
-  if(e.getKeyChar()=='l'){
-     lightPos= new float[]{lightPos[0]+0.1f, lightPos[1]+0.1f,lightPos[2]+0.1f,lightPos[3]+0.01f};
-     }
- }
- public void keyReleased(KeyEvent e){}
- public void keyTyped(KeyEvent e){}
- });
+        frame.addKeyListener(new KeyListener() {
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    xrot -= 1.0f;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    xrot += 1.0f;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    yrot += 1.0f;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    yrot -= 1.0f;
+                }
+
+                if (e.getKeyChar() == 'q') {
+                    ambientLight = new float[]{ambientLight[0] - 0.1f, ambientLight[1] - 0.1f, ambientLight[2] - 0.1f, ambientLight[3] - 0.01f};
+                }
+                if (e.getKeyChar() == 'w') {
+                    ambientLight = new float[]{ambientLight[0] + 0.1f, ambientLight[1] + 0.1f, ambientLight[2] + 0.1f, ambientLight[3] + 0.01f};
+                }
+                if (e.getKeyChar() == 'a') {
+                    diffuseLight = new float[]{diffuseLight[0] - 0.1f, diffuseLight[1] - 0.1f, diffuseLight[2] - 0.1f, diffuseLight[3] - 0.01f};
+                }
+                if (e.getKeyChar() == 's') {
+                    diffuseLight = new float[]{diffuseLight[0] + 0.1f, diffuseLight[1] + 0.1f, diffuseLight[2] + 0.1f, diffuseLight[3] + 0.01f};
+                }
+                if (e.getKeyChar() == 'z') {
+                    specular = new float[]{specular[0] - 0.1f, specular[1] - 0.1f, specular[2] - 0.1f, specular[3] - 0.01f};
+                }
+                if (e.getKeyChar() == 'x') {
+                    specular = new float[]{specular[0] + 0.1f, specular[1] + 0.1f, specular[2] + 0.1f, specular[3] + 0.01f};
+                }
+                if (e.getKeyChar() == 'k') {
+                    lightPos = new float[]{lightPos[0] - 0.1f, lightPos[1] - 0.1f, lightPos[2] - 0.1f, lightPos[3] - 0.01f};
+                }
+                if (e.getKeyChar() == 'l') {
+                    lightPos = new float[]{lightPos[0] + 0.1f, lightPos[1] + 0.1f, lightPos[2] + 0.1f, lightPos[3] + 0.01f};
+                }
+            }
+
+            public void keyReleased(KeyEvent e) {
+            }
+
+            public void keyTyped(KeyEvent e) {
+            }
+        });
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         animator.start();
-   
-        
-        
-        
+
     }
 
     public void init(GLAutoDrawable drawable) {
@@ -116,25 +117,24 @@ public class Marekszmolda implements GLEventListener {
         // Enable VSync
         gl.setSwapInterval(1);
 
-    //warto?ci sk?adowe o?wietlenia i koordynaty ?ród?a ?wiat?a
-    
+        //warto?ci sk?adowe o?wietlenia i koordynaty ?ród?a ?wiat?a
         //(czwarty parametr okre?la odleg?o?? ?ród?a:
         //0.0f-niesko?czona; 1.0f-okre?lona przez pozosta?e parametry)
         gl.glEnable(GL.GL_LIGHTING); //uaktywnienie o?wietlenia
         //ustawienie parametrów ?ród?a ?wiat?a nr. 0
-        gl.glLightfv(GL.GL_LIGHT0,GL.GL_AMBIENT,ambientLight,0); //swiat?o otaczaj?ce
-        gl.glLightfv(GL.GL_LIGHT0,GL.GL_DIFFUSE,diffuseLight,0); //?wiat?o rozproszone
-        gl.glLightfv(GL.GL_LIGHT0,GL.GL_SPECULAR,specular,0); //?wiat?o odbite
-        gl.glLightfv(GL.GL_LIGHT0,GL.GL_POSITION,lightPos,0); //pozycja ?wiat?a
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, ambientLight, 0); //swiat?o otaczaj?ce
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, diffuseLight, 0); //?wiat?o rozproszone
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_SPECULAR, specular, 0); //?wiat?o odbite
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, lightPos, 0); //pozycja ?wiat?a
         gl.glEnable(GL.GL_LIGHT0); //uaktywnienie ?ród?a ?wiat?a nr. 0
         gl.glEnable(GL.GL_COLOR_MATERIAL); //uaktywnienie ?ledzenia kolorów
         //kolory b?d? ustalane za pomoc? glColor
         gl.glColorMaterial(GL.GL_FRONT, GL.GL_AMBIENT_AND_DIFFUSE);
         //Ustawienie jasno?ci i odblaskowo?ci obiektów
-        float specref[] = { 1.0f, 1.0f, 1.0f, 1.0f }; //parametry odblaskowo?ci
-        gl.glMaterialfv(GL.GL_FRONT, GL.GL_SPECULAR,specref,0);
-        
-        gl.glMateriali(GL.GL_FRONT,GL.GL_SHININESS,128);
+        float specref[] = {1.0f, 1.0f, 1.0f, 1.0f}; //parametry odblaskowo?ci
+        gl.glMaterialfv(GL.GL_FRONT, GL.GL_SPECULAR, specref, 0);
+
+        gl.glMateriali(GL.GL_FRONT, GL.GL_SHININESS, 128);
 
         gl.glEnable(GL.GL_DEPTH_TEST);
         // Setup the drawing area and shading mode
@@ -142,13 +142,12 @@ public class Marekszmolda implements GLEventListener {
         gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
     }
 
-
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
         GL gl = drawable.getGL();
         GLU glu = new GLU();
 
         if (height <= 0) { // avoid a divide by zero error!
-        
+
             height = 1;
         }
         final float h = (float) width / (float) height;
@@ -160,6 +159,67 @@ public class Marekszmolda implements GLEventListener {
         gl.glLoadIdentity();
     }
 
+    void stozek(GL gl) {
+//wywo?ujemy automatyczne normalizowanie normalnych
+        gl.glEnable(GL.GL_NORMALIZE);
+        float x, y, kat;
+        gl.glBegin(GL.GL_TRIANGLE_FAN);
+        gl.glVertex3f(0.0f, 0.0f, -2.0f); //wierzcholek stozka
+        for (kat = 0.0f; kat < (2.0f * Math.PI); kat += (Math.PI / 32.0f)) {
+            x = (float) Math.sin(kat);
+            y = (float) Math.cos(kat);
+            gl.glNormal3f((float) Math.sin(kat), (float) Math.cos(kat), -2.0f);
+            gl.glVertex3f(x, y, 0.0f);
+        }
+        gl.glEnd();
+        gl.glBegin(GL.GL_TRIANGLE_FAN);
+        gl.glNormal3f(0.0f, 0.0f, 1.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f); //srodek kola
+        for (kat = 2.0f * (float) Math.PI; kat > 0.0f; kat -= (Math.PI / 32.0f)) {
+            x = (float) Math.sin(kat);
+            y = (float) Math.cos(kat);
+            gl.glVertex3f(x, y, 0.0f);
+        }
+        gl.glEnd();
+
+    }
+
+    void walec(GL gl) {
+//wywo?ujemy automatyczne normalizowanie normalnych
+//bo operacja skalowania je zniekszta?ci
+        gl.glEnable(GL.GL_NORMALIZE);
+        float x, y, kat;
+        gl.glBegin(GL.GL_QUAD_STRIP);
+        for (kat = 0.0f; kat < (2.0f * Math.PI); kat += (Math.PI / 32.0f)) {
+            x = 0.5f * (float) Math.sin(kat);
+            y = 0.5f * (float) Math.cos(kat);
+            gl.glNormal3f((float) Math.sin(kat), (float) Math.cos(kat), 0.0f);
+            gl.glVertex3f(x, y, -1.0f);
+            gl.glVertex3f(x, y, 0.0f);
+        }
+        gl.glEnd();
+        gl.glNormal3f(0.0f, 0.0f, -1.0f);
+        x = y = kat = 0.0f;
+        gl.glBegin(GL.GL_TRIANGLE_FAN);
+        gl.glVertex3f(0.0f, 0.0f, -1.0f); //srodek kola
+        for (kat = 0.0f; kat < (2.0f * Math.PI); kat += (Math.PI / 32.0f)) {
+            x = 0.5f * (float) Math.sin(kat);
+            y = 0.5f * (float) Math.cos(kat);
+            gl.glVertex3f(x, y, -1.0f);
+        }
+        gl.glEnd();
+        gl.glNormal3f(0.0f, 0.0f, 1.0f);
+        x = y = kat = 0.0f;
+        gl.glBegin(GL.GL_TRIANGLE_FAN);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f); //srodek kola
+        for (kat = 2.0f * (float) Math.PI; kat > 0.0f; kat -= (Math.PI / 32.0f)) {
+            x = 0.5f * (float) Math.sin(kat);
+            y = 0.5f * (float) Math.cos(kat);
+            gl.glVertex3f(x, y, 0.0f);
+        }
+        gl.glEnd();
+    }
+
     public void display(GLAutoDrawable drawable) {
         GL gl = drawable.getGL();
 
@@ -167,22 +227,27 @@ public class Marekszmolda implements GLEventListener {
         gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
         // Reset the current matrix to the "identity"
         gl.glLoadIdentity();
-        gl.glTranslatef(0.0f, 0.0f, -9.0f); //przesuni?cie o 6 jednostek
- gl.glRotatef(xrot, 1.0f, 0.0f, 0.0f); //rotacja wokó? osi X
- gl.glRotatef(yrot, 0.0f, 1.0f, 0.0f); //rotacja wokó? osi Y
- 
-  gl.glEnable(GL.GL_LIGHTING); //uaktywnienie o?wietlenia
-        //ustawienie parametrów ?ród?a ?wiat?a nr. 0
-        gl.glLightfv(GL.GL_LIGHT0,GL.GL_AMBIENT,ambientLight,0); //swiat?o otaczaj?ce
-        gl.glLightfv(GL.GL_LIGHT0,GL.GL_DIFFUSE,diffuseLight,0); //?wiat?o rozproszone
-        gl.glLightfv(GL.GL_LIGHT0,GL.GL_SPECULAR,specular,0); //?wiat?o odbite
-        gl.glLightfv(GL.GL_LIGHT0,GL.GL_POSITION,lightPos,0); //pozycja ?wiat?a
-        gl.glEnable(GL.GL_LIGHT0); //uaktywnienie ?ród?a ?wiat?a nr. 0
-        gl.glEnable(GL.GL_COLOR_MATERIAL); 
-        
-        drzewko(gl);
+        gl.glTranslatef(0.0f, 0.0f, -20.0f); //przesuni?cie o 6 jednostek
+        gl.glRotatef(xrot, 1.0f, 0.0f, 0.0f); //rotacja wokó? osi X
+        gl.glRotatef(yrot, 0.0f, 1.0f, 0.0f); //rotacja wokó? osi Y
 
-     
+        gl.glEnable(GL.GL_LIGHTING); //uaktywnienie o?wietlenia
+        //ustawienie parametrów ?ród?a ?wiat?a nr. 0
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, ambientLight, 0); //swiat?o otaczaj?ce
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, diffuseLight, 0); //?wiat?o rozproszone
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_SPECULAR, specular, 0); //?wiat?o odbite
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, lightPos, 0); //pozycja ?wiat?a
+        gl.glEnable(GL.GL_LIGHT0); //uaktywnienie ?ród?a ?wiat?a nr. 0
+        gl.glEnable(GL.GL_COLOR_MATERIAL);
+
+        for(int i=0; i<10 ;i++)
+        {
+            drzewko(gl);
+            gl.glTranslatef(4.0f, 0.0f, 0.0f);
+            drzewko(gl);
+            gl.glTranslatef(-4.0f, 4.0f, 0.0f);
+        }
+
 //gl.glBegin(GL.GL_QUADS);
 ////?ciana przednia
 //gl.glColor3f(1.0f,0.0f,0.0f);
@@ -222,9 +287,7 @@ public class Marekszmolda implements GLEventListener {
 //gl.glVertex3f(1.0f,1.0f,-1.0f);
 //gl.glVertex3f(-1.0f,1.0f,-1.0f);
 //gl.glEnd();
- 
 //ostroslup
- 
 // gl.glBegin(GL.GL_QUADS);
 // gl.glColor3f(0.0f,0.5f,1.0f);
 //gl.glVertex3f(-1.0f,-1.0f,1.0f);
@@ -254,7 +317,7 @@ public class Marekszmolda implements GLEventListener {
 //gl.glVertex3f(-1.0f,-1.0f,1.0f);
 //gl.glVertex3f(1.0f,-1.0f,1.0f);
 //gl.glEnd(); 
- //walec
+        //walec
 // float x,y,kat;
 //gl.glBegin(GL.GL_TRIANGLE_FAN);
 //gl.glVertex3f(0.0f,1.0f, .0f); //?rodek
@@ -288,97 +351,35 @@ public class Marekszmolda implements GLEventListener {
 // gl.glVertex3f(x2,-1.0f,y2);//kolejne punkty
 //}
 // gl.glEnd();       // Flush all drawing operations to the graphics card
-      //  gl.glFlush();
-       
-  //drzewko      
-     
+        //  gl.glFlush();
+        //drzewko      
     }
-    
-    void drzewko(GL gl){
-           gl.glColor3f(0.0f,1.0f,0.0f);
+
+    void drzewko(GL gl) {
+         gl.glPushMatrix();
+        gl.glColor3f(0.0f, 1.0f, 0.0f);
         stozek(gl);
-        
-          gl.glScalef(1.2f, 1.2f, 1.0f);
-        gl.glTranslatef(0.0f,0.0f,1.0f);
+        gl.glColor3f(0.0f, 1.0f, 0.0f);
         stozek(gl);
-        
-          gl.glScalef(1.4f, 1.4f, 1.0f);
-         gl.glTranslatef(0.0f,0.0f,1.0f);
+
+        gl.glScalef(1.2f, 1.2f, 1.0f);
+        gl.glTranslatef(0.0f, 0.0f, 1.0f);
         stozek(gl);
-        
-        gl.glColor3f(64/256f, 32/256f,0.0f);
-          gl.glScalef(0.7f, 0.7f, 1.0f);
-         gl.glTranslatef(0.0f,0.0f,1.0f);
+
+        gl.glScalef(1.4f, 1.4f, 1.0f);
+        gl.glTranslatef(0.0f, 0.0f, 1.0f);
+        stozek(gl);
+
+        gl.glColor3f(0.210f, 0.105f, 0.0f);
+        gl.glScalef(0.7f, 0.7f, 1.0f);
+        gl.glTranslatef(0.0f, 0.0f, 1.0f);
         walec(gl);
+        gl.glPopMatrix();
+        
+        
+        
     }
     
-    void walec(GL gl)
- {
-//wywo?ujemy automatyczne normalizowanie normalnych
-//bo operacja skalowania je zniekszta?ci
-gl.glEnable(GL.GL_NORMALIZE);
-float x,y,kat;
-gl.glBegin(GL.GL_QUAD_STRIP);
-for(kat = 0.0f; kat < (2.0f*Math.PI); kat += (Math.PI/32.0f))
-{
-x = 0.5f*(float)Math.sin(kat);
-y = 0.5f*(float)Math.cos(kat);
-gl.glNormal3f((float)Math.sin(kat),(float)Math.cos(kat),0.0f);
-gl.glVertex3f(x, y, -1.0f);
-gl.glVertex3f(x, y, 0.0f);
-}
-gl.glEnd();
-gl.glNormal3f(0.0f,0.0f,-1.0f);
-x=y=kat=0.0f;
-gl.glBegin(GL.GL_TRIANGLE_FAN);
-gl.glVertex3f(0.0f, 0.0f, -1.0f); //srodek kola
-for(kat = 0.0f; kat < (2.0f*Math.PI); kat += (Math.PI/32.0f))
-{
-x = 0.5f*(float)Math.sin(kat);
-y = 0.5f*(float)Math.cos(kat);
-gl.glVertex3f(x, y, -1.0f);
-}
-gl.glEnd();
-gl.glNormal3f(0.0f,0.0f,1.0f);
-x=y=kat=0.0f;
-gl.glBegin(GL.GL_TRIANGLE_FAN);
-gl.glVertex3f(0.0f, 0.0f, 0.0f); //srodek kola
-for(kat = 2.0f*(float)Math.PI; kat > 0.0f ; kat -= (Math.PI/32.0f))
-{
-x = 0.5f*(float)Math.sin(kat);
-y = 0.5f*(float)Math.cos(kat);
-gl.glVertex3f(x, y, 0.0f);
-}
-gl.glEnd();
-}
-
-void stozek(GL gl)
-{
-//wywo?ujemy automatyczne normalizowanie normalnych
-gl.glEnable(GL.GL_NORMALIZE);
-float x,y,kat;
-gl.glBegin(GL.GL_TRIANGLE_FAN);
-gl.glVertex3f(0.0f, 0.0f, -2.0f); //wierzcholek stozka
-for(kat = 0.0f; kat < (2.0f*Math.PI); kat += (Math.PI/32.0f))
-{
-x = (float)Math.sin(kat);
-y = (float)Math.cos(kat);
-gl.glNormal3f((float)Math.sin(kat),(float)Math.cos(kat),-2.0f);
-gl.glVertex3f(x, y, 0.0f);
-}
-gl.glEnd();
-gl.glBegin(GL.GL_TRIANGLE_FAN);
-gl.glNormal3f(0.0f,0.0f,1.0f);
-gl.glVertex3f(0.0f, 0.0f, 0.0f); //srodek kola
-for(kat = 2.0f*(float)Math.PI; kat > 0.0f; kat -= (Math.PI/32.0f))
-{
-x = (float)Math.sin(kat);
-y = (float)Math.cos(kat);
-gl.glVertex3f(x, y, 0.0f);
-}
-gl.glEnd();
-
-}
     
     public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
     }
